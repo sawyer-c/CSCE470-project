a845f0dc61bd8dfb3981ce2f4111e94d03f26178
sawyer-c <sawyer_cowan818@yahoo.com>
2020-11-17 16:17:30 -0600

    added print to see when the response is done

--- test.py
+++ test.py
@@ -2,5 +2,6 @@ import requests
 import json
 url = "http://127.0.0.1:5000/"
 
-response = requests.get(url+"vsm/ford_mustang")
+response = requests.get(url+"vsm/roadster_is_the_best")
 print(response.json())
+print("\n***Response Received!***\n")
